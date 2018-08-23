9ac1022c5674e8050c038d048930531687a94a52
cevatbarisyilmaz <cevatbarisyilmaz@gmail.com>
2018-08-23 18:59:57 +0300

    Check for nil pointer for listener attribute

--- pconn.go
+++ pconn.go
@@ -20,29 +20,32 @@ func (pConn *pConn) Write(b []byte) (int, error) {
 }
 
 func (pConn *pConn) Close() error {
-	pConn.listener.connCond.L.Lock()
-	pConn.listener.currentConn--
-	pConn.listener.connCond.L.Unlock()
-	tcpAddr, err := net.ResolveTCPAddr("tcp", pConn.tcpConn.RemoteAddr().String())
-	if err == nil {
-		var ip [16]byte
-		copy(ip[:], tcpAddr.IP.To16())
-		record := pConn.listener.getRecord(ip)
-		record.mut.Lock()
-		defer record.mut.Unlock()
-		if len(record.activeConns) > 0 {
-			var index int
-			for i, c := range record.activeConns {
-				if c == pConn {
-					index = i
-					break
+	myListener := pConn.listener
+	if myListener != nil {
+		myListener.connCond.L.Lock()
+		myListener.currentConn--
+		myListener.connCond.L.Unlock()
+		tcpAddr, err := net.ResolveTCPAddr("tcp", pConn.tcpConn.RemoteAddr().String())
+		if err == nil {
+			var ip [16]byte
+			copy(ip[:], tcpAddr.IP.To16())
+			record := myListener.getRecord(ip)
+			record.mut.Lock()
+			defer record.mut.Unlock()
+			if len(record.activeConns) > 0 {
+				var index int
+				for i, c := range record.activeConns {
+					if c == pConn {
+						index = i
+						break
+					}
 				}
+				record.activeConns[index] = record.activeConns[len(record.activeConns)-1]
+				record.activeConns = record.activeConns[:len(record.activeConns)-1]
 			}
-			record.activeConns[index] = record.activeConns[len(record.activeConns)-1]
-			record.activeConns = record.activeConns[:len(record.activeConns)-1]
 		}
+		(*myListener.connCond).Signal()
 	}
-	(*pConn.listener.connCond).Signal()
 	return pConn.tcpConn.Close()
 }
 
@@ -66,7 +69,9 @@ func (pConn *pConn) SetWriteDeadline(t time.Time) error {
 	return pConn.tcpConn.SetWriteDeadline(t)
 }
 
-//CastToTCPConn casts a net.Conn created by plistener to a *net.TCPConn.
+// CastToTCPConn casts a net.Conn created by plistener to a *net.TCPConn.
+// All functions of *net.TCPConn can be used freely, except the Close function.
+// Close always should called via the original net.Conn returned by PListener.
 func CastToTCPConn(conn net.Conn) (*net.TCPConn, error) {
 	a, ok := conn.(*pConn)
 	if !ok {
